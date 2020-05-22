8ff414f1bb1451e4566c03bea7d8dabba81b7550
Thomas Bley <mails@thomasbley.de>
2020-05-22 15:38:09 +0200

    added tasks controller and repository

---
+++ app/Controllers/TasksController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace TaskService\Controllers;
+
+use DateTime;
+use TaskService\Exceptions\HttpException;
+use TaskService\Framework\App;
+use TaskService\Models\Customer;
+use TaskService\Models\Task;
+
+class TasksController
+{
+    protected $app;
+
+    public function __construct(App $app)
+    {
+        $this->app = $app;
+    }
+
+    public function createTask(Customer $customer, string $title, string $duedate): Task
+    {
+        if (empty($title)) {
+            throw new HttpException('missing title', 400);
+        }
+        if (!DateTime::createFromFormat('Y-m-d', $duedate)) {
+            throw new HttpException('invalid duedate', 400);
+        }
+
+        $repo = $this->app->getTasksRepository();
+
+        return $repo->createTask($customer, $title, $duedate);
+    }
+
+    public function updateTask(Customer $customer, Task $task): Task
+    {
+        if (empty($task->title)) {
+            throw new HttpException('missing title', 400);
+        }
+        if (!DateTime::createFromFormat('Y-m-d', $task->duedate)) {
+            throw new HttpException('invalid duedate', 400);
+        }
+
+        $repo = $this->app->getTasksRepository();
+
+        if (!$repo->taskExists($customer, $task->id)) {
+            throw new HttpException('task not found', 404);
+        }
+
+        $repo->updateTask($task);
+    }
+
+    public function deleteTask(Customer $customer, int $taskId): void
+    {
+        $repo = $this->app->getTasksRepository();
+
+        if (!$repo->taskExists($customer, $taskId)) {
+            throw new HttpException('task not found', 404);
+        }
+
+        $repo->deleteTask($taskId);
+    }
+
+    /**
+     * @return Task[]
+     */
+    public function getCurrentTasks(Customer $customer): array
+    {
+        $repo = $this->app->getTasksRepository();
+
+        return $repo->getCurrentTasks($customer);
+    }
+
+    /**
+     * @return Task[]
+     */
+    public function getCompletedTasks(Customer $customer): array
+    {
+        $repo = $this->app->getTasksRepository();
+
+        return $repo->getCompletedTasks($customer);
+    }
+
+    public function getTask(Customer $customer, int $taskId): Task
+    {
+        $repo = $this->app->getTasksRepository();
+
+        $task = $repo->getTask($customer, $taskId);
+
+        if (empty($task)) {
+            throw new HttpException('task not found', 404);
+        }
+
+        return $task;
+    }
+}
---
+++ app/Repositories/TasksRepository.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace TaskService\Repositories;
+
+use PDO;
+use TaskService\Framework\App;
+use TaskService\Models\Customer;
+use TaskService\Models\Task;
+
+class TasksRepository
+{
+    protected $app;
+
+    public function __construct(App $app)
+    {
+        $this->app = $app;
+    }
+
+    public function taskExists(Customer $customer, int $taskId): bool
+    {
+        $query = '
+            SELECT id FROM task WHERE id = ? AND customer_id = ?
+        ';
+        $statement = $this->app->getDatabase()->prepare($query);
+        $statement->execute([$taskId, $customer->id]);
+
+        return !empty($statement->fetchColumn());
+    }
+
+    public function createTask(Customer $customer, string $title, string $duedate): Task
+    {
+        $query = '
+            INSERT INTO task SET customer_id = ?, title = ?, duedate = ?, completed = 0
+        ';
+        $db = $this->app->getDatabase();
+        $statement = $db->prepare($query);
+        $statement->execute([$customer->id, $title, $duedate]);
+
+        $task = new Task();
+        $task->id = (int) $db->lastInsertId();
+        $task->title = $title;
+        $task->duedate = $duedate;
+        $task->completed = false;
+
+        return $task;
+    }
+
+    public function updateTask(Task $task): void
+    {
+        $query = '
+            UPDATE task SET title = ?, duedate = ?, completed = ? WHERE id = ?
+        ';
+        $db = $this->app->getDatabase();
+        $statement = $db->prepare($query);
+        $statement->execute([$task->title, $task->duedate, (int) $task->completed, $task->id]);
+    }
+
+    public function deleteTask(int $taskId): void
+    {
+        $query = '
+            DELETE FROM task WHERE id = ?
+        ';
+        $this->app->getDatabase()->prepare($query)->execute([$taskId]);
+    }
+
+    public function getTask(Customer $customer, int $taskId): ?Task
+    {
+        $db = $this->app->getDatabase();
+
+        $query = '
+            SELECT id, title, duedate, completed FROM task WHERE customer_id = ? AND id = ?
+        ';
+        $statement = $db->prepare($query);
+        $statement->execute([$customer->id, $taskId]);
+
+        return $statement->fetchObject(Task::class) ?: null;
+    }
+
+    /**
+     * @return Task[]
+     */
+    public function getCurrentTasks(Customer $customer): array
+    {
+        $db = $this->app->getDatabase();
+
+        $query = '
+            SELECT id, title, duedate, completed FROM task
+            WHERE customer_id = ? AND completed = 0 AND duedate < ?
+            ORDER BY duedate
+            LIMIT 500
+        ';
+        $statement = $db->prepare($query);
+        $statement->execute([$customer->id, date('Y-m-d', strtotime('+1 week'))]);
+
+        return $statement->fetchAll(PDO::FETCH_CLASS, Task::class);
+    }
+
+    /**
+     * @return Task[]
+     */
+    public function getCompletedTasks(Customer $customer): array
+    {
+        $db = $this->app->getDatabase();
+
+        $query = '
+            SELECT id, title, duedate, completed FROM task
+            WHERE customer_id = ? AND completed = 1
+            ORDER BY duedate DESC
+            LIMIT 500
+        ';
+        $statement = $db->prepare($query);
+        $statement->execute([$customer->id]);
+
+        return $statement->fetchAll(PDO::FETCH_CLASS, Task::class);
+    }
+}
