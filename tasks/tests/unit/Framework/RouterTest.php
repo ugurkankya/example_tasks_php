06ef377c1139f3af7d9eec1ec82d337c1655a375
Thomas Bley <mails@thomasbley.de>
2021-05-25 22:54:25 +0200

    cleanup

--- tasks/tests/unit/Framework/RouterTest.php
+++ tasks/tests/unit/Framework/RouterTest.php
@@ -15,10 +15,11 @@ class RouterTest extends TestCase
 
         $router = new Router();
 
-        $router->post('/v1/api/123/abc', function() use (&$never): void { $never = true; });
-        $router->get('/v1/api/other', function() use (&$never): void { $never = true; });
-        $router->get('/v1/api/(\d+)/(\w+)', function(int $i, string $s) use (&$called, &$with): void {
-            $called = true; $with = [$i, $s];
+        $router->post('/v1/api/123/abc', function () use (&$never): void { $never = true; });
+        $router->get('/v1/api/other', function () use (&$never): void { $never = true; });
+        $router->get('/v1/api/(\d+)/(\w+)', function (int $i, string $s) use (&$called, &$with): void {
+            $called = true;
+            $with = [$i, $s];
         });
 
         $router->match('GET', '/v1/api/123/abc');
@@ -35,9 +36,9 @@ class RouterTest extends TestCase
 
         $router = new Router();
 
-        $router->post('/v1/api/abc', function() use (&$never): void { $never = true; });
-        $router->put('/v1/api/other', function() use (&$never): void { $never = true; });
-        $router->put('/v1/api/abc', function() use (&$called): void { $called = true; });
+        $router->post('/v1/api/abc', function () use (&$never): void { $never = true; });
+        $router->put('/v1/api/other', function () use (&$never): void { $never = true; });
+        $router->put('/v1/api/abc', function () use (&$called): void { $called = true; });
 
         $router->match('PUT', '/v1/api/abc');
 
@@ -52,9 +53,9 @@ class RouterTest extends TestCase
 
         $router = new Router();
 
-        $router->put('/v1/api/abc', function() use (&$never): void { $never = true; });
-        $router->post('/v1/api/other', function() use (&$never): void { $never = true; });
-        $router->post('/v1/api/abc', function() use (&$called): void { $called = true; });
+        $router->put('/v1/api/abc', function () use (&$never): void { $never = true; });
+        $router->post('/v1/api/other', function () use (&$never): void { $never = true; });
+        $router->post('/v1/api/abc', function () use (&$called): void { $called = true; });
 
         $router->match('POST', '/v1/api/abc');
 
@@ -69,9 +70,9 @@ class RouterTest extends TestCase
 
         $router = new Router();
 
-        $router->post('/v1/api/abc', function() use (&$never): void { $never = true; });
-        $router->patch('/v1/api/other', function() use (&$never): void { $never = true; });
-        $router->patch('/v1/api/abc', function() use (&$called): void { $called = true; });
+        $router->post('/v1/api/abc', function () use (&$never): void { $never = true; });
+        $router->patch('/v1/api/other', function () use (&$never): void { $never = true; });
+        $router->patch('/v1/api/abc', function () use (&$called): void { $called = true; });
 
         $router->match('PATCH', '/v1/api/abc');
 
@@ -86,9 +87,9 @@ class RouterTest extends TestCase
 
         $router = new Router();
 
-        $router->patch('/v1/api/abc', function() use (&$never): void { $never = true; });
-        $router->delete('/v1/api/other', function() use (&$never): void { $never = true; });
-        $router->delete('/v1/api/abc', function() use (&$called): void { $called = true; });
+        $router->patch('/v1/api/abc', function () use (&$never): void { $never = true; });
+        $router->delete('/v1/api/other', function () use (&$never): void { $never = true; });
+        $router->delete('/v1/api/abc', function () use (&$called): void { $called = true; });
 
         $router->match('DELETE', '/v1/api/abc');
 
@@ -103,9 +104,9 @@ class RouterTest extends TestCase
 
         $router = new Router();
 
-        $router->any('/v1/api/test', function() use (&$never): void { $never = true; });
-        $router->any('/v1/api/abc', function() use (&$called): void { $called = true; });
-        $router->any('/v1/api/abc', function() use (&$never): void { $never = true; });
+        $router->any('/v1/api/test', function () use (&$never): void { $never = true; });
+        $router->any('/v1/api/abc', function () use (&$called): void { $called = true; });
+        $router->any('/v1/api/abc', function () use (&$never): void { $never = true; });
 
         $router->match('GET', '/v1/api/abc');
 
@@ -121,11 +122,12 @@ class RouterTest extends TestCase
 
         $router = new Router();
 
-        $router->any('example\.php', function() use (&$never): void { $never = true; });
-        $router->any('example\.php (\w+)', function(string $s) use (&$called, &$with): void {
-            $called = true; $with = $s;
+        $router->any('example\.php', function () use (&$never): void { $never = true; });
+        $router->any('example\.php (\w+)', function (string $s) use (&$called, &$with): void {
+            $called = true;
+            $with = $s;
         });
-        $router->any('example\.php', function() use (&$never): void { $never = true; });
+        $router->any('example\.php', function () use (&$never): void { $never = true; });
 
         $router->match('', 'example.php 123');
 
